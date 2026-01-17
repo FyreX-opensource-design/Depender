c43a48fb53aa72d3e2f50f81a08aeb573ff87324
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2026-01-17 10:02:38 -0600

    Update and rename install.sh to install-system.sh

---
+++ install-system.sh
@@ -0,0 +1,5 @@
+#!/user/bin/env bash
+
+chmod +x /opt/apps/depender/dli.py
+sudo ln -s /opt/apps/depender/dli.py /usr/bin/depender
+
--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/user/bin/env bash
-
-chmod +x ./dli.py
-sudo mkdir -p /opt/system/lib
-sudo mv ./dli.py /opt/system/lib/
-sudo ln -s ./dli.py /usr/bin/depender
