cd09fd7c1313e32a68586481ae442f72cb6d8aee
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2026-01-17 13:39:00 -0600

    Update install-system.sh

--- install-system.sh
+++ install-system.sh
@@ -1,4 +1,5 @@
 #!/user/bin/env bash
 
-sudo chmod +x ./dli.py
-sudo ln -s ./dli.py /usr/bin/depender
+sudo chmod +x /opt/apps/depender/dli.py
+sudo ln -s /opt/apps/depender/dli.py /usr/bin/depender
+
