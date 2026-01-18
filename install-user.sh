d6a09367e63ab65c7b3cf90e1a9d74a50a7cfcb2
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2026-01-18 10:00:57 -0600

    Update install-user.sh

--- install-user.sh
+++ install-user.sh
@@ -2,4 +2,4 @@
 
 chmod +x ~/.local/share/apps/depender/dli.py
 chmod +x ~/.local/share/apps/depender/depender-user.sh
-sudo ln -s ~/.local/share/apps/depender/depender-user.sh /usr/bin/depender
+ln -s ~/.local/share/apps/depender/depender-user.sh /usr/bin/depender
